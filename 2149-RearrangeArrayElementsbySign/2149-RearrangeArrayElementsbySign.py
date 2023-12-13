ebbaab0217530d9e379cb98bf987f14f6b3c826b
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2023-12-13 18:23:18 +0530

    Create file

---
+++ 2149-RearrangeArrayElementsbySign/2149-RearrangeArrayElementsbySign.py
@@ -0,0 +1,15 @@
+                positive.append(i)
+            else:
+                negative.append(i)
+        for i in range(len(nums)//2):
+            result.append(positive[i])
+            if i > 0:
+        for i in nums:
+
+        negative = []
+        result = []
+            result.append(negative[i])
+
+        return result
+
+[
