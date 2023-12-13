38e2fa5d4637f8a1ade7f487ce0eefbe68afa194
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2023-12-13 16:51:07 +0530

    Create file

---
+++ 1295-FindNumberswithEvenNumberofDigits/1295-FindNumberswithEvenNumberofDigits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        count = 0
+        for i in nums:
+            if len(str(i)) % 2 == 0:
+                count +=1
+        return count
+        
+[
