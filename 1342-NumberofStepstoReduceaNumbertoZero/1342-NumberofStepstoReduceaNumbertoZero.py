63dd599f7357db06cf3424d2b14469d9967d789b
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2023-12-13 22:31:37 +0530

    Create file

---
+++ 1342-NumberofStepstoReduceaNumbertoZero/1342-NumberofStepstoReduceaNumbertoZero.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        step = 0
+        while num != 0:
+            if num % 2 == 0:
+                num //= 2
+                step += 1
+            else:
+                num -= 1
+                step += 1
+        return step
+
+        
+1
