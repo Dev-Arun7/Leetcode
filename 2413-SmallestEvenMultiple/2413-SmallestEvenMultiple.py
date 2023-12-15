59562faa715c3f8781294ac8fe5c49c771807b27
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2023-12-15 13:47:42 +0530

    Create file

---
+++ 2413-SmallestEvenMultiple/2413-SmallestEvenMultiple.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        i = n
+        while True:
+            if i % n == 0 and i % 2 == 0:
+            else:
+                break
+                i += n
+        return i
+5
