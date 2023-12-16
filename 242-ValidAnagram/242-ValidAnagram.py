2177c82d5e35f67d9ebc02174e9a0811ff54f49c
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2023-12-16 07:55:35 +0530

    Create file

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,15 @@
+            else:
+        return flag
+                return False
+
+                continue
+            if (i in s) and (t.count(i) <= s.count(i)):
+        for i in t:
+        if len(s) != len(t):
+        flag = True
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+            return False
+                
+        
+"
