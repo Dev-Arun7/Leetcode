a75d0cad3c5ed3592b14d2d599d20848081197d0
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2023-12-25 18:36:17 +0530

    Create file

---
+++ 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        altitude = [0]
+        for i in gain:
+            temp += i
+        temp = 0
+            altitude.append(temp)
+        return max(altitude)
+        
+[
