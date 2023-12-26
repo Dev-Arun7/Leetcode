3d5da41619e90afc590fc8d4cda1b5463cf7ccf5
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2023-12-26 13:00:09 +0530

    Create file

---
+++ 2496-MaximumValueofaStringinanArray/2496-MaximumValueofaStringinanArray.py
@@ -0,0 +1,18 @@
+                    _isAlpha = True
+                        value = len(i)
+            else:
+                if int(i) > value:
+                if j.isalpha():
+            for j in i:
+        for i in strs:
+        value = 0
+            _isAlpha = False
+    def maximumValue(self, strs: List[str]) -> int:
+class Solution:
+                    break
+            if _isAlpha:
+                if len(i) > value:
+                    value = int(i)           
+        return value
+        
+[
