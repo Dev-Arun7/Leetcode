3482244cad846c29b134310d24469d9c993f44eb
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2023-12-26 15:45:07 +0530

    Create file

---
+++ 2511-MaximumEnemyFortsThatCanBeCaptured/2511-MaximumEnemyFortsThatCanBeCaptured.py
@@ -0,0 +1,13 @@
+                    temp = 0
+            if i == 1:
+                start_a = True
+                temp = 0
+            elif i == -1:
+                start_b = True
+                temp = 0
+
+
+        return fort
+            
+        
+[
