c46a4357338b3639fedbd5fac7a74c454ca94901
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2023-12-28 19:33:03 +0530

    Create file

---
+++ 1450-NumberofStudentsDoingHomeworkataGivenTime/1450-NumberofStudentsDoingHomeworkataGivenTime.py
@@ -0,0 +1,5 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        return sum(1 for i in range(len(startTime)) if queryTime >= startTime[i] and queryTime <= endTime
+[i])
+[
