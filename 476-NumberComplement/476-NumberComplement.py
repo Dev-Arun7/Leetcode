5914988ecac9bfe004de1bd469de532c201499fc
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2023-12-31 20:32:25 +0530

    Create file

---
+++ 476-NumberComplement/476-NumberComplement.py
@@ -0,0 +1,13 @@
+        flip = ""
+        for i in bin_str:
+            if i == "1":
+                flip += '0'
+            else:
+                flip += '1'
+        return int(flip, 2)
+
+        bin_str = str(bin_num)[2:]
+        bin_num = bin(num)
+    def findComplement(self, num: int) -> int:
+class Solution:
+5
