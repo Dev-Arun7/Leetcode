a981435439e856a7eee8accbc6b4b3c91bbe17c1
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2023-12-31 22:01:05 +0530

    Create file

---
+++ 500-KeyboardRow/500-KeyboardRow.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        set1 = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'}
+        set2 = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'}
+
+        for i in words:
+            if set(lower).issubset(set1) or set(lower).issubset(set2) or set(lower).issubset
+                result.append(i)
+        set3 = {'z', 'x', 'c', 'v', 'b', 'n', 'm'}
+        result = []
+        return result
+
+            lower = i.lower()
+(set3):
+        
+[
