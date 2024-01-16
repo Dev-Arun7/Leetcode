af14d294871e8636df60f89a07f4fdc79867e187
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-01-16 17:51:34 +0530

    Create file

---
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.py
@@ -0,0 +1,13 @@
+class·Solution:
+····def·findMaxConsecutiveOnes(self,·nums:·List[int])·->·int:
+········count·=·temp·=·0
+········for·i·in·nums:
+············if·i·==·1:
+················temp·+=·1
+············else:
+················count·=·max(count,·temp)
+················temp·=·0
+········count·=·max(count,·temp)
+········return·count
+········
+[
