6afac0e35e60caa9a3fc4c785edc32d3ffb41b8c
Dev-Arun7 <123arunblakrishnan@gmail.com>
2024-01-17 15:23:04 +0530

    updating files

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        word_list=s.split()
-        return len(word_list[-1])
-        
--- 0075-sort-colors/0075-sort-colors.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        nums.sort()
-       
-        
