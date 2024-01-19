5293b5b023aef5e307d67823b7d49d5a307a6c73
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-01-19 18:05:41 +0530

    Create file

---
+++ 2194-CellsinaRangeonanExcelSheet/2194-CellsinaRangeonanExcelSheet.py
@@ -0,0 +1,17 @@
+class Solution:
+    def cellsInRange(self, s: str) -> list[str]:
+        c1 = ord(s[0])
+        c2 = ord(s[3])
+        r1 = int(s[1])
+        r2 = int(s[4])
+
+        for i in range(c1, c2+1):  # Adjusted the order of iteration
+                result.append(cell_details)
+        result = []
+            for j in range(r1, r2+1):  # Adjusted the order of iteration
+                col_char = chr(i)
+                cell_details = f"{col_char}{j}"
+
+        return result
+
+"
