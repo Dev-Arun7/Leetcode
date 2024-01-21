b1f8cb889ae21814e2a1a174b21b4387527f8637
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-01-21 12:07:27 +0530

    Update file 1/21/2024, 12:07:26 PM

--- 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.py
+++ 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.py
@@ -1,10 +1,7 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        letters = set(string.ascii_lowercase)
-        for i in letters:
-            if i not in sentence_set:
-                return False
-        sentence_set = set(sentence)
-        return True
+
+        sentence = set(sentence)
+        return len(sentence) == 26
         
 "
