c527fe033ff52c3bbb814da26769110618e4dbf6
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-01-22 18:52:26 +0530

    Update file 1/22/2024, 6:52:25 PM

--- 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.py
+++ 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.py
@@ -1,7 +1,7 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
 
-        sentence = set(sentence)
-        return len(sentence) == 26
+        return len(set(sentence)) == 26
+
         
 "
