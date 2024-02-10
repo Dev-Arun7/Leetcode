058425df4e9414750b7a0c6a4b5103a9640e06a9
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-02-10 22:47:23 +0530

    Create file

---
+++ 1725-NumberOfRectanglesThatCanFormTheLargestSquare/1725-NumberOfRectanglesThatCanFormTheLargestSquare.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
+        ans = 0
+        for i in rectangles:
+            temp = min(i)
+            if temp > ans:
+        return count
+        count = 0
+                ans = temp
+                count = 1
+            elif temp == ans:
+                count += 1
+        
+[
