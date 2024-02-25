59e3fa3329492a5ba15790d44305a5d52f0165fc
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-02-25 12:51:21 +0530

    Create file

---
+++ 1779-FindNearestPointThatHastheSameXorYCoordinate/1779-FindNearestPointThatHastheSameXorYCoordinate.py
@@ -0,0 +1,15 @@
+        result = float('inf')
+        for i, point in enumerate(points):
+            if point[0] == x or point[1] == y:
+                temp = abs(x - point[0]) + abs(y - point[1])
+                if result > temp:
+class Solution:
+    def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
+                    distance = i
+        
+        return distance
+
+        distance = -1
+                    result = temp
+        
+3
