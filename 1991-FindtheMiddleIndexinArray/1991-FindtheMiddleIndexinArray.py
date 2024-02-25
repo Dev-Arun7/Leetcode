3c342d9c86d11344c401db07fd90787a193cc8ff
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-02-25 23:22:18 +0530

    Create file

---
+++ 1991-FindtheMiddleIndexinArray/1991-FindtheMiddleIndexinArray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        _sum = 0
+        else:
+        l = len(nums)
+            for i in range(l):
+                    _sum += nums[i-1]
+                if _sum == total - (_sum + nums[i]):
+                if i -1 > -1:
+                else:
+                    _sum += 0
+········if·len(nums)·==·1:
+············return·0
+
+                    return i
+        return -1      
+[
