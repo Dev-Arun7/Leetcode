ee3c87705ab9af3ad7268f5ddd32bc455ccfa53a
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-03-03 23:01:44 +0530

    Create file

---
+++ 1880-CheckifWordEqualsSummationofTwoWords/1880-CheckifWordEqualsSummationofTwoWords.py
@@ -0,0 +1,17 @@
+        for i in firstWord:
+            k=k+str(h.index(i))
+        for i in secondWord:
+            a+=str(h.index(i))
+        for i in targetWord:
+            e+=str(h.index(i))
+        if int(k)+int(a)==int(e):
+            return True
+        else:
+        h=string.ascii_lowercase
+        a=''
+        e=''
+        import string
+        k=''
+class Solution:
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+"
