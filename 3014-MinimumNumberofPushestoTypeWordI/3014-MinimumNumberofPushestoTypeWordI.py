4a69162e274c83d9ac262f5f75189e38b78c921e
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-03-12 20:56:21 +0530

    Update file 3/12/2024, 8:56:20 PM

--- 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.py
+++ 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.py
@@ -1,12 +1,13 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
         if len(word) < 9:
-            return len(word)
+            return n
         elif len(word) < 17:
-            return 8 + (2 * (len(word) - 8))
+            return 8 + (2 * (n - 8))
         elif len(word) < 25:
-            return 24 + (3 * (len(word) - 16))
+            return 24 + (3 * (n - 16))
         else:
-            return 48 + (4*(len(word)-24))
+            return 48 + (4* (n-24))
+        n = len(word)
         
 "
