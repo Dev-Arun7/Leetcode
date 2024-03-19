61eb137803179e662e2265af51dfde52a3cb34c1
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-03-19 17:49:31 +0530

    Create file

---
+++ 520-DetectCapital/520-DetectCapital.py
@@ -0,0 +1,26 @@
+
+            return True
+        else:
+            else:
+                return False
+                    return True
+
+        if upper:
+            for i in range(1, l):
+                if word[i].islower():
+                    return False
+        elif lower:
+            for i in range(1, l):
+                if word[i].isupper():
+                    return False
+        else:
+            for i in range(1, l):
+                if word[i].isupper():
+                    return False
+
+        return True
+
+
+
+        
+"
