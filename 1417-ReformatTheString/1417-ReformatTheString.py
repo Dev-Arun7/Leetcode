efb93eabd3fb4f92ba52ee9d11c2cc5b679e1d67
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-03-20 22:50:51 +0530

    Create file

---
+++ 1417-ReformatTheString/1417-ReformatTheString.py
@@ -0,0 +1,22 @@
+                if i != l-1:
+                    ans += letters[i] + digits[i]
+                else:
+                    ans += letters[i]
+            for i in range(d):
+                if i != d-1:
+            for i in range(l):
+        elif l > d:
+            return ans
+        exist = [-1, 0, 1]
+        if (l-d) not in exist:
+        elif l == d:
+        else:
+            for i in range(l):
+                ans += letters[i] + digits[i]
+                    ans += digits[i] + letters[i]
+                else:
+                    ans += digits[i]
+        return ans
+
+        
+"
