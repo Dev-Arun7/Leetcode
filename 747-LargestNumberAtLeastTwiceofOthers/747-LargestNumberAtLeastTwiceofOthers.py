a35d4903b457a72fd994b4d25c0008faba42b33b
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-04-21 19:17:14 +0530

    Create file

---
+++ 747-LargestNumberAtLeastTwiceofOthers/747-LargestNumberAtLeastTwiceofOthers.py
@@ -0,0 +1,17 @@
+        for i, num in enumerate(nums):
+            if num > first:
+                first = num
+            elif num > second:
+                second = num
+        
+        if first >= 2 * second:
+                index = i
+            return index
+        index = 0
+        second = float('-inf')
+        else:
+        first = float('-inf')
+    def dominantIndex(self, nums: List[int]) -> int:
+class Solution:
+                second = first
+[
