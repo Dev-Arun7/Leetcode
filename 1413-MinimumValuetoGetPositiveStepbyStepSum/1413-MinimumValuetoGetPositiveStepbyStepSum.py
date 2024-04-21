7218af2880ad48cdf1fd004a1424cf665f2e1ebc
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-04-21 12:56:28 +0530

    Create file

---
+++ 1413-MinimumValuetoGetPositiveStepbyStepSum/1413-MinimumValuetoGetPositiveStepbyStepSum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+        total = 0
+        for num in nums:
+            total += num
+        
+        if _min < 0:
+            return abs(_min) + 1
+        _min = 0
+            _min = min(_min, total)
+        else:
+            return 1
+        
+[
