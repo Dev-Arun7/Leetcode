d4a0798735708d5b63c912be4e757560ef5f7f74
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-05-01 17:47:56 +0530

    Create file

---
+++ 2390-RemovingStarsFromaString/2390-RemovingStarsFromaString.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        ans = []
+        for letter in s:
+                ans.append(letter)
+        return ans
+            if letter == '*':
+                ans.pop()
+            else:
+        ans = ''.join(ans)
+        
+"
