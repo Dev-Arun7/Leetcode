39a40d9b286b1f227147f19b58503ef43b5e169f
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-06-04 20:43:01 +0530

    Update file 04/06/2024, 20:43:01

--- 409-LongestPalindrome/409-LongestPalindrome.py
+++ 409-LongestPalindrome/409-LongestPalindrome.py
@@ -1,4 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
+        my_set = set()
+        my_list = []
+        even = 0
+        odd = 0
+        for i in s:
+            my_set.add(i)
+            my_list.append(i)
+        for i in my_set:
+            temp = my_list.count(i)
+            if temp % 2 == 0:
+                even += temp
+            else:
+                even += temp
+                even -= 1
+                if temp > odd:
+                    odd = temp
+        if odd:
+            even += 1
+        return even
         
 "abccccdd"
