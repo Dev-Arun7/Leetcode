7cdd7bfc813c8f653c6a936892739ae7d8badc58
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-06-11 18:44:00 +0530

    Update file 11/06/2024, 18:44:00

--- 1122-RelativeSortArray/1122-RelativeSortArray.py
+++ 1122-RelativeSortArray/1122-RelativeSortArray.py
@@ -1,16 +1,25 @@
+            if i in arr2_set: nums1.append(i)
+            else: nums2.append(i)
+        d = Counter(nums1)
+
         ans = []
         for i in arr2:
             ans += [i] * d[i]
         return ans + sorted(nums2) 
 
-            if i in arr2_set: nums1.append(i)
-            else: nums2.append(i)
-        d = Counter(nums1)
 
-        for i in arr1:
-        nums2 = []
+
+
+
+
+
+
+
+
         nums1 = []
-        arr2_set = set(arr2)
-    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        nums2 = []
+        for i in arr1:
 class Solution:
-[
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        arr2_set = set(arr2)
+[2,3,1,3,2,4,6,7,9,2,19]
