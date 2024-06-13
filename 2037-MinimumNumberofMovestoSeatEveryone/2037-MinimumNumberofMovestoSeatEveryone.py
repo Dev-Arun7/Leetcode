2a6bbe41854f0a201d23aac22caccdc4371d6641
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-06-13 20:45:01 +0530

    Update file 13/06/2024, 20:45:00

--- 2037-MinimumNumberofMovestoSeatEveryone/2037-MinimumNumberofMovestoSeatEveryone.py
+++ 2037-MinimumNumberofMovestoSeatEveryone/2037-MinimumNumberofMovestoSeatEveryone.py
@@ -3,12 +3,11 @@ from typing import List
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
         count = 0
-
-        for i in range(len(seats)):
-            count += abs(seats[i] - students[i])
         seats.sort()
         students.sort()
 
-        return count
+        for i in range(len(seats)):
+            count += abs(seats[i] - students[i])
 
+        return count
 [
