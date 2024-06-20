f61ee51ca985b4506b47f4af36d9d86589d870d3
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-06-20 18:23:12 +0530

    Update file 20/06/2024, 18:23:11

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -2,8 +2,7 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         unique_nums = list(set(nums))  # Create a list with unique elements
         nums.clear()  # Clear the original list
-        nums.extend(unique_nums)  # Add the unique elements back to the original list
+        nums.extend(unique_nums)  
         nums.sort()
         return len(nums)
-
 [
