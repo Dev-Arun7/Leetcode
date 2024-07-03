423c021af33b2ba0b9c6e8f081874719a5380fda
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-07-03 20:14:59 +0530

    Update file 03/07/2024, 20:14:59

--- 350-IntersectionofTwoArraysII/350-IntersectionofTwoArraysII.py
+++ 350-IntersectionofTwoArraysII/350-IntersectionofTwoArraysII.py
@@ -1,21 +1,13 @@
+
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        count = Counter(nums1)
         result = []
-        hash_1 = {}
-        hash_2 = {}
-        for n in nums1:
-            hash_1[n] = hash_1.get(n, 0) + 1
-
         
         for n in nums2:
-            hash_2[n] = hash_2.get(n, 0) + 1
-
-        for n in hash_1:
-            temp = min(hash_1[n], hash_2.get(n, 0))
-            if temp > 0:
-                for i in range(temp):
-                    result.append(n)
-
-        return result
+            if count[n] > 0:
+                result.append(n)
+                count[n] -= 1
         
+        return result
 [
