84b7276473744960019224aa0249110221351213
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-07-20 11:30:58 +0530

    Create file

---
+++ 2243-CalculateDigitSumofaString/2243-CalculateDigitSumofaString.py
@@ -0,0 +1,12 @@
+                else:
+                    sum_ += int(digit)
+                    count +=1
+                    sum_ += int(digit)
+                    count +=1
+            tempDigits += str(sum_)
+            digits = tempDigits
+        return digits
+            
+
+        
+"
