b9fb658933b2c651d10e4767804c19e5c2c77c3c
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-08-03 10:10:00 +0530

    Update file 03/08/2024, 10:10:00

--- 1460-MakeTwoArraysEqualbyReversingSubarrays/1460-MakeTwoArraysEqualbyReversingSubarrays.py
+++ 1460-MakeTwoArraysEqualbyReversingSubarrays/1460-MakeTwoArraysEqualbyReversingSubarrays.py
@@ -1,22 +1,31 @@
-class Solution:
-    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        target_map = {}
-        arr_map = {}
-        for num in target:
-            if num in target_map:
-                target_map[num] += 1
-            else:
-                target_map[num] = 1
-        for num in arr:
-            if num in arr_map:
-                arr_map[num] += 1
-            else:
-                arr_map[num] = 1
-        for num in target_map:
-            if target_map[num] != arr_map.get(num, 0):
+        #     if num in arr_map:
+        #         arr_map[num] += 1
+        #     else:
+        #         arr_map[num] = 1
+        # for num in target_map:
+        #     if target_map[num] != arr_map.get(num, 0):
+        #         return False
+        # return True
+        
+        count1, count2 = defaultdict(int), defaultdict(int)
+        for n1, n2 in zip(target, arr):
+            count1[n1] += 1
+            count2[n2] += 1
+
+        for n in count1:
+            if count1[n] != count2[n]:
                 return False
         return True
-        
+        # for num in arr:
+        #         target_map[num] += 1
+        #     else:
+        #         target_map[num] = 1
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        # target_map = {}
+        # arr_map = {}
+        # for num in target:
+        #     if num in target_map:
 [
 [1,2,3,4]
 [2,4,1,3]
