3056de48e643bdd9dee5115c4b5b1dfc42e736b6
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-09-13 21:13:44 +0530

    Create file

---
+++ 877-StoneGame/877-StoneGame.py
@@ -0,0 +1,29 @@
+        #     if piles[left] >= piles[right]:
+        #         big = piles[left]
+        #         left += 1
+        #     else:
+        #         big = piles[right]
+        #         right -= 1
+        #     if i % 2 == 1:
+        #         alice += big
+        #     else:
+        #         bob += big
+        # while left < right:
+        # right = len(piles) - 1
+        # i = 1
+        # left = 0
+        # bob = 0
+        # alice = 0
+    def stoneGame(self, piles: List[int]) -> bool:
+        # return True if alice > bob else False
+class Solution:
+        #     i += 1
+        return True
+
+[
+[5,3,4,5]
+[3,7,2,3]
+true
+true
+true
+true
